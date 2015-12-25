28955e5f23365aa9814ce2bd82d42715d68bebc2
abrarmusa <abrar.musa.89@gmail.com>
2015-12-25 07:06:11 +0600

    added cors

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var dataRoutes = require('./routes/dataRoutes');
+var cors = require('cors')
 
 
 var app = express();
@@ -15,11 +16,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
